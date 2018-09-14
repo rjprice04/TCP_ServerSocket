79cfecf65d84daadbde2a7115c1394f348b8242d
Ryan <ryan-price@uiowa.edu>
2018-09-13 21:41:28 -0500

    Initial Commit

---
+++ TCP_SocketServer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TCP_SocketServer
+//
+//  Created by Ryan Price on 9/13/18.
+//  Copyright © 2018 Ryan Price. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
