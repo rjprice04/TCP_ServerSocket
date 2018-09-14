e76149a357f382e24593ed04bee3f5d60621f932
Ryan <ryan-price@uiowa.edu>
2018-09-13 22:52:50 -0500

    Server Side

--- TCP_SocketServer/main.c
+++ TCP_SocketServer/main.c
@@ -7,9 +7,40 @@
 //
 
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#define SIZE 1024
+char buf[SIZE];
+#define TIME_PORT 2013
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    char serverMessage[256] = "You got the server!";
+    //creat the socket
+    int netWorkSocket = socket(AF_INET, SOCK_STREAM, 0);
+    
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(9003);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    //bind the socket
+    int bindStatus = bind(netWorkSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress));
+    if(bindStatus == -1){
+        printf("Error\n\n");
+    }
+    else{
+        printf("good bind\n");
+    }
+
+    listen(netWorkSocket, 5);
+    //Accept
+    int clientSocket = accept(netWorkSocket, NULL, NULL);
+    send(clientSocket, serverMessage, sizeof(serverAddress), 0);
+    shutdown(netWorkSocket, SHUT_RDWR);
     return 0;
 }
